4c08f04acf1d0366dd7f96f329774096258b4939
K0ch3rga <mr.skel@mail.ru>
2024-08-08 11:51:36 +0500

    removed: unplugin dependency

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import VueRouter from 'unplugin-vue-router/vite'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    VueRouter({}),
     vue({
       template: { transformAssetUrls },
     }),
